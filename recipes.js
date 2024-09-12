d2b020876056abbbff637224dfa0945e569709d2
Maylo0314 <maylo.elfrink@student.graafschapcollege.nl>
2024-09-12 09:43:09 +0200

    Fixed Recipe search bug

--- recipes.js
+++ recipes.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     $('#searchButton').on('click', function () {
         $.ajax({
-            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=c25e2d349ba842ee8186ded1ff30b942`
+            url: `https://api.spoonacular.com/recipes/complexSearch?query=${$('#searchValue').val()}&apiKey=1d0e17601e9a41f9aa5f60b6c1622fd4`
         }).done(function (data) {
+            const searchResults = document.querySelector('.searchResults');
+            searchResults.innerHTML = "";
+            
             data.results.forEach(recipe => {
-                const searchResults = document.querySelector('.searchResults');
-
                 const newItem = document.createElement('div');
                 newItem.classList.add('recipe');
                 newItem.innerHTML = `<h3>${recipe.title}</h3><a href="ingredients.html?id=${recipe.id}"><img src=${recipe.image} /></a>`;
